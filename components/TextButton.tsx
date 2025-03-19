60483be332fcf9b7929eb13af914e866d6256c30
tewodros-m <tewodrosmengie@gmail.com>
2025-03-19 16:06:31 +0300

    feat: add TextButton component with customizable styles and color theme

---
+++ components/TextButton.tsx
@@ -0,0 +1,57 @@
+import { useColorTheme } from '@/hooks/useColorTheme';
+import React, { useMemo } from 'react';
+import {
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  TouchableOpacityProps,
+  TextStyle,
+} from 'react-native';
+
+interface TextButtonProps extends TouchableOpacityProps {
+  title: string;
+  titleStyle?: TextStyle;
+  onPress: () => void;
+}
+
+const TextButton: React.FC<TextButtonProps> = ({
+  title,
+  titleStyle,
+  onPress,
+  style,
+  ...props
+}) => {
+  const colors = useColorTheme();
+
+  const styles = useMemo(
+    () =>
+      StyleSheet.create({
+        button: {
+          paddingVertical: 12,
+          paddingHorizontal: 16,
+          borderRadius: 12,
+          alignItems: 'center',
+          justifyContent: 'center',
+          backgroundColor: colors.primary,
+        },
+        title: {
+          fontWeight: '700',
+          color: colors['gray-100'],
+          fontSize: 16,
+        },
+      }),
+    []
+  );
+
+  return (
+    <TouchableOpacity
+      onPress={onPress}
+      style={[styles.button, style]}
+      {...props}
+    >
+      <Text style={[styles.title, titleStyle]}>{title}</Text>
+    </TouchableOpacity>
+  );
+};
+
+export default TextButton;
